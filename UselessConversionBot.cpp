ee3b6a57dc84748b8eebdab33e6823e5678dc73c
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2023-09-19 10:12:24 -0400

    Add files via upload

---
+++ UselessConversionBot.cpp
@@ -0,0 +1,25 @@
+/* Class: Assignment2C
+Section: c++
+Term: S2
+Instructor: Kavitha Muppa
+Name: Ibrahima Diallo
+Lab#: E01 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main () {
+  float distance;
+  int bonbon;
+  int frigo;
+
+  cout << "Enter a number of miles walked:";
+  cin >> distance;
+  bonbon = 11787.9072575*distance;
+  frigo = 945.67*distance;
+  cout << "\nI walked a total of " << distance << " today #fitnessgoals" << endl;
+  cout << "Usless Conversion bot: Wow, " << distance << " miles is " << frigo << " refrigerators long." << endl;
+  cout << "Usless Conversion bot: " << distance << " miles is also " << bonbon << " candy bars long." << endl;
+  cout << "Usless Conversion bot: That's a lot!"; 
+}
