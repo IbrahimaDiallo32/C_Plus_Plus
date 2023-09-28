a9c7d51179db1a1c7f4204be1da88e7b29efaf12
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2023-09-28 10:54:14 -0400

    Add files via upload

---
+++ OverloadedSort.cpp
@@ -0,0 +1,103 @@
+/*Class: Assignment6C
+Section: c++
+Term: S2
+Instructor: Kavitha Muppa
+Name: Ibrahima Diallo
+Lab#: E01 */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sortArray(int array[8]){
+for (int i=0; i < 8-1;i++){
+  for (int j=0; j< 8-i-1;j++){
+    if (array[j] > array[j+1]){
+      int temporary= array[j];
+      array[j]=array[j+1];
+      array[j+1]=temporary;
+    }
+    
+    }
+}
+  for (int i=0;i<=7;i++){
+    if (array[i] == array[7]){
+      cout << array[7];
+      break;
+    }
+    cout << array[i] << ",";
+    
+  }
+  }
+void sortArray(float array[8]){
+for (int i=0; i < 8-1;i++){
+  for (int j=0; j< 8-i-1;j++){
+    if (array[j] > array[j+1]){
+      float temporary= array[j];
+      array[j]=array[j+1];
+      array[j+1]=temporary;
+    }
+    
+    }
+}
+  for (int i=0;i<=7;i++){
+    if (array[i] == array[7]){
+      cout << array[7];
+      break;
+    }
+    cout << array[i] << ",";
+    
+  }
+  }
+void sortArray(char array[8]){
+  for (int i=0; i < 8-1;i++){
+  for (int j=0; j< 8-i-1;j++){
+    if (array[j] > array[j+1]){
+      char temporary= array[j];
+      array[j]=array[j+1];
+      array[j+1]=temporary;
+    }
+    
+    }
+
+}
+  for (int i=0;i<=7;i++){
+    cout << array[i]<<",";
+  }
+  }
+
+
+int main () {
+  string datatype;
+  cout << "[Overloaded Sort]\nWhat data type do you want to enter?";
+    cin>>datatype;
+  
+  if (datatype == "int" || datatype == "Int"){
+    int array[8];
+  for (int i=0;i<=7;i++){
+    cout << "Value " << i+1 << ": ";
+    cin >> array[i];
+    }
+    cout << "Calling sortArray()...\nThe sorted values are:"<<endl;
+    sortArray(array);
+    }
+    else if (datatype == "float" || datatype == "Float"){
+    float array[8];
+  for (int i=0;i<=7;i++){
+    cout << "Value " << i+1 << ": ";
+    cin >> array[i];
+    }
+      cout << "Calling sortArray()...\nThe sorted values are:"<<endl;
+      sortArray(array);
+      }
+  else{
+    char array[8];
+  for (int i=0;i<=7;i++){
+    cout << "Value " << i+1 << ": ";
+    cin >> array[i];
+  }
+    cout << "Calling sortArray()...\nThe sorted values are:"<<endl;
+    sortArray(array);
+    }
+
+}
